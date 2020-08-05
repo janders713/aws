ac315faee85dfb9811b98e591e363a3570f4c754
janders713 <68660499+janders713@users.noreply.github.com>
2020-08-05 15:04:24 -0400

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,37 @@
+import boto3
+
+client = boto3.client("lambda")
+
+
+resp1 = client.create_function(
+    FunctionName='glueFunc',
+    Runtime='python3.8',
+    Role='arn:aws:iam::555494021232:role/lambda',
+    Handler='code1.lambda_handler',
+    Code={
+        'S3Bucket': 'jacob713-bucket',
+        'S3Key': 'lambda_func.zip',
+    },
+)
+
+resp2 = client.create_function(
+    FunctionName='dynamodbFunc',
+    Runtime='python3.8',
+    Role='arn:aws:iam::555494021232:role/lambda',
+    Handler='code2.query_customers',
+    Code={
+        'S3Bucket': 'jacob713-bucket',
+        'S3Key': 'lambda_func.zip',
+    },
+)
+
+resp3 = client.create_function(
+    FunctionName='timeFunc',
+    Runtime='python3.8',
+    Role='arn:aws:iam::555494021232:role/lambda,
+    Handler='code3.date_conv',
+    Code={
+        'S3Bucket': 'jacob713-bucket',
+        'S3Key': 'lambda_func.zip',
+    },
+)
