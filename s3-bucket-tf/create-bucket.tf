6b4a5e964695ad4c5a6dee13d3e5c9f700fc6f05
janders713 <68660499+janders713@users.noreply.github.com>
2020-08-12 15:29:51 -0400

    Update create-bucket.tf

--- s3-bucket-tf/create-bucket.tf
+++ s3-bucket-tf/create-bucket.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "JacobAnderson1996" {
     bucket = "JacobAnderson1996"
     acl = "private"
 }
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "tf_course" {
 
 #uploading a file to the bucket
 resource "aws_s3_bucket_object" "object" {
-  bucket = "tf_course"
+  bucket = "JacobAnderson1996"
   key    = "MOCK_DATA"
   source = "C:/Users/Owner/Downloads/MOCK_DATA.csv"
 
