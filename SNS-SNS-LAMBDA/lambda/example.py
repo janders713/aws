b666087c85c513f4ba7561bc85f791716c3956ad
janders713 <68660499+janders713@users.noreply.github.com>
2020-08-31 17:58:32 -0400

    Add files via upload

---
+++ SNS-SNS-LAMBDA/lambda/example.py
@@ -0,0 +1,12 @@
+import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.environ)
+    logger.info('## EVENT')
+    logger.info(event)
